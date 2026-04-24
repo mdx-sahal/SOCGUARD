ddabf8ea760d56e444e3eec26fbb2a1cbfafef13
mdx-sahal <sahalhii5581@gmail.com>
2026-04-24 20:14:45 +0530

    Delete check_cuda.py

--- check_cuda.py
+++
@@ -1,8 +0,0 @@
-
-import torch
-print(f"Torch Version: {torch.__version__}")
-print(f"CUDA Available: {torch.cuda.is_available()}")
-if torch.cuda.is_available():
-    print(f"Device Name: {torch.cuda.get_device_name(0)}")
-else:
-    print("Running on CPU")
